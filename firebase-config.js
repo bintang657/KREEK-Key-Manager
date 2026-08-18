8f662ba4620815463e826bbd07bf2a93952aac16
bintang657 <dwatu8720@gmail.com>
2026-08-19 01:27:47 +0700

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -3,14 +3,14 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth
 import { getDatabase } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBiqyKpz1Jp0FGSx28kb9H7jwSjOVW-mtc",
+ apiKey: "AIzaSyBiqyKpz1Jp0FGSx28kb9H7jwSjOVW-mtc",
   authDomain: "kreek-4aa08.firebaseapp.com",
-  databaseURL: "https://kreek-4aa08-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  databaseURL: "https://kreek-4aa08-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "kreek-4aa08",
   storageBucket: "kreek-4aa08.firebasestorage.app",
   messagingSenderId: "617654659028",
-  appId: "1:617654659028:web:1139afe57ae7f4e324d841",
-  measurementId: "G-TX5TCY679N"
+  appId: "1:617654659028:web:83bf7ce3bee2afbf24d841",
+  measurementId: "G-R9SRVPJ5N8"
 };
 
 export const app = initializeApp(firebaseConfig);
